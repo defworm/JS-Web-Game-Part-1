60aa81c1a5732f3552a7b2cd79ae90d291aab7d2
Crystal Korth <crystalkorth@gmail.com>
2022-05-30 22:04:57 -0500

    added items

--- index.js
+++ index.js
@@ -17,6 +17,28 @@ function newItem (url, left, bottom){
 }
 
 newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+// let sword = document.createElement ('img')
+// sword.src = 'assets/sword.png'
+// sword.position = 'fixed'
+// sword.left = '500px'
+// sword.bottom = '405px'
+// document.body.append (sword)
+
+// sword.addEventListener ('click', function () {
+//     sword.remove ()
+// })
+
+
 
 
 // let greenCharacter = document.createElement('img')
